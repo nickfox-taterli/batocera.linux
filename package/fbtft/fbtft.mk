b18340e3c2d2e241c27aff489f5e4fc0c82c9651
Tater Li <admin@lijingquan.net>
2017-05-10 16:48:00 +0800

    Update fbtft.mk

--- package/fbtft/fbtft.mk
+++ package/fbtft/fbtft.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-FBTFT_VERSION = 274035404701245e7491c0c6471c5b72ade4d491
-FBTFT_SITE = $(call github,notro,fbtft,$(FBTFT_VERSION))
+FBTFT_VERSION = 109683227c5c093d9272064bad9f9ba8152ead57
+FBTFT_SITE = $(call github,nickfox-taterli,fbtft,$(FBTFT_VERSION))
 FBTFT_LICENSE = GPL-2.0
 
 $(eval $(generic-package))
